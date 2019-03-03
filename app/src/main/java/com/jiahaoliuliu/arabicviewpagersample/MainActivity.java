3b82475d96b0b5e32e828dee1da0971226598bda
Jiahao Liu <jiahao.liu@dnata.com>
2019-03-03 11:52:13 +0400

    Set the view pager for arabic

--- app/src/main/java/com/jiahaoliuliu/arabicviewpagersample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/arabicviewpagersample/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TableLayout;
 
 import java.util.Locale;
 
